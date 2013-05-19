53f1f76584615dbe77a1ada0d9603fc03a10949f
Ankur Oberoi <aoberoi@gmail.com>
2013-05-19 18:11:52 -0400

    more

--- public/js/opentok.js
+++ public/js/opentok.js
@@ -1,3 +1,7 @@
+TB.addEventListener('exception', function(error) {
+});
+
+
 var session = TB.initSession('1_MX4yOTY3NzA4Mn4xMjcuMC4wLjF-U3VuIE1heSAxOSAxMTo0OTo0MCBQRFQgMjAxM34wLjg0NjQxMjN-');
 
 session.addEventListener('sessionConnected', function(e) {
@@ -16,4 +20,11 @@ function addStreams(streams) {
   }
 }
 
-session.connect('29677082', 'T1==cGFydG5lcl9pZD0yOTY3NzA4MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1lYjdjMWFjZGZmNzA3MTQwOWE0MjMzNjJhZWQyZTZlOWUwZTNmZjYzOnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9MV9NWDR5T1RZM056QTRNbjR4TWpjdU1DNHdMakYtVTNWdUlFMWhlU0F4T1NBeE1UbzBPVG8wTUNCUVJGUWdNakF4TTM0d0xqZzBOalF4TWpOLSZjcmVhdGVfdGltZT0xMzY4OTg5NDUzJm5vbmNlPTAuMzM1MDMxNDgwMzk3MTExNSZleHBpcmVfdGltZT0xMzY5MDc1ODUyJmNvbm5lY3Rpb25fZGF0YT0=');
+
+function connectToSession() {
+  session.connect('29677082', 'T1==cGFydG5lcl9pZD0yOTY3NzA4MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1lYjdjMWFjZGZmNzA3MTQwOWE0MjMzNjJhZWQyZTZlOWUwZTNmZjYzOnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9MV9NWDR5T1RZM056QTRNbjR4TWpjdU1DNHdMakYtVTNWdUlFMWhlU0F4T1NBeE1UbzBPVG8wTUNCUVJGUWdNakF4TTM0d0xqZzBOalF4TWpOLSZjcmVhdGVfdGltZT0xMzY4OTg5NDUzJm5vbmNlPTAuMzM1MDMxNDgwMzk3MTExNSZleHBpcmVfdGltZT0xMzY5MDc1ODUyJmNvbm5lY3Rpb25fZGF0YT0=');
+}
+
+$('#connect').on('click', function(e) {
+  connectToSession();
+});
